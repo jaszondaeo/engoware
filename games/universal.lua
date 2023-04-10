7a140cae4f335154842a2d04dd2011b64d8e72d1
jaszondaeo <128945088+jaszondaeo@users.noreply.github.com>
2023-04-10 14:10:38 +0900

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -32,8 +32,8 @@ do
                     end
 
                     if SpeedOptions.Values.pulse.Enabled then 
-                        if AddSpeed > (SpeedValue.Value * 1.45) then
-                            AddSpeed = -(SpeedValue.Value * 1.01)
+                        if AddSpeed > (SpeedValue.Value * 1.7) then
+                            AddSpeed = -(SpeedValue.Value * 1.5)
                         else
                             AddSpeed = AddSpeed + 1
                         end
